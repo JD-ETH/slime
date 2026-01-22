ec60338099306d73fe62fc3b7eb0fb27123c470d
JensenFire <elevenji@outlook.com>
2026-01-23 00:39:27 +0800

    cudagraph only 1

--- tests/test_weight_transfer_moe_multinode_qwen235b_16nodes.py
+++ tests/test_weight_transfer_moe_multinode_qwen235b_16nodes.py
@@ -195,7 +195,7 @@ def execute(args: ScriptArgs):
         f"--sglang-dp-size {args.sglang_dp} "
         f"--sglang-ep-size {args.sglang_ep} "
         "--sglang-enable-dp-lm-head "
-        "--sglang-cuda-graph-bs 1 2 4 8 16 24 32 40 48 56 64 "
+        "--sglang-cuda-graph-bs 1 "
         # "--sglang-moe-a2a-backend deepep "
         # "--sglang-deepep-mode auto "
         f"--sglang-inter-node-transfer-engine-info-port {args.inter_node_transfer_engine_info_port} "
