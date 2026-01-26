0ea1f0c61bc03d29c3ce8f092611f528be445090
JensenFire <elevenji@outlook.com>
2026-01-26 06:59:13 +0000

    mixc

--- tests/test_weight_transfer_moe_multinode_qwen235b_16nodes.py
+++ tests/test_weight_transfer_moe_multinode_qwen235b_16nodes.py
@@ -204,8 +204,8 @@ def execute(args: ScriptArgs):
     )
     if args.mode == "rdma":
         sglang_args += "--sglang-remote-instance-weight-loader-start-seed-via-transfer-engine "
-    if args.pipelined_transfer and args.mode == "rdma":
-        sglang_args += "--rdma-pipelined-transfer "
+    # if args.pipelined_transfer and args.mode == "rdma":
+    #     sglang_args += "--rdma-pipelined-transfer "
 
     # ci_args = "--ci-test "
 
